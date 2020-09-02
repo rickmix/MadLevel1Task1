bbc9ab2c6d53b86bc4ff76f6e56e85d1079f6cac
Rick Schut <rick-schut@live.nl>
2020-09-02 10:49:11 +0200

    Added first functions

--- app/src/main/java/com/example/madlevel1task1/HigherLowerActivity.kt
+++ app/src/main/java/com/example/madlevel1task1/HigherLowerActivity.kt
@@ -2,10 +2,29 @@ package com.example.madlevel1task1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.madlevel1task1.databinding.ActivityHigherLowerBinding
 
 class HigherLowerActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityHigherLowerBinding
+    private var currentThrow: Int = 1
+    private var lastThrow: Int = 1
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_higher_lower)
+        binding = ActivityHigherLowerBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        initViews()
     }
+
+    // Set initial UI state
+    private fun initViews() {
+        updateUI()
+    }
+
+    // Update last throw text and image with current throw
+    private fun updateUI() {
+        binding.underDiceText.text = getString(R.string.last_throw, lastThrow)
+    }
+
 }
